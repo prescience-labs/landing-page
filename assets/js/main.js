d2e2d2adffb305e5451573ab3c4424c6810eb3e3
CorrOrtiz <a.ortizcrr@gmail.com>
2019-08-24 12:40:23 -0500

    axios post

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,129 +169,11 @@ $(document).ready(function($) {
   }
 });
 
-const moreExamples = {
-  id: '4cd00d8d-d7e2-4cef-bd29-20a2e85b3919',
-  text: 'The staff is always happy and helpful',
-  sentiment_analysis: {
-    model: 'general_en',
-    score_tag: 'P',
-    agreement: 'AGREEMENT',
-    subjectivity: 'SUBJECTIVE',
-    confidence: '100',
-    irony: 'NONIRONIC',
-    sentence_list: [
-      {
-        text: 'The staff is always happy and helpful',
-        inip: '0',
-        endp: '36',
-        bop: 'y',
-        confidence: '100',
-        score_tag: 'P',
-        agreement: 'AGREEMENT',
-        segment_list: [
-          {
-            text: 'The staff is always happy and helpful',
-            segment_type: 'main',
-            inip: '0',
-            endp: '36',
-            confidence: '100',
-            score_tag: 'P',
-            agreement: 'AGREEMENT',
-            polarity_term_list: [
-              {
-                text: 'happy',
-                inip: '20',
-                endp: '24',
-                confidence: '100',
-                score_tag: 'P',
-                sentimented_concept_list: [
-                  {
-                    form: 'staff',
-                    id: '9431a28626',
-                    variant: 'staff',
-                    inip: '4',
-                    endp: '8',
-                    type: 'Top>Organization',
-                    score_tag: 'P',
-                  },
-                ],
-              },
-              {
-                text: 'helpful',
-                inip: '30',
-                endp: '36',
-                confidence: '100',
-                score_tag: 'P',
-                sentimented_concept_list: [
-                  {
-                    form: 'staff',
-                    id: '9431a28626',
-                    variant: 'staff',
-                    inip: '4',
-                    endp: '8',
-                    type: 'Top>Organization',
-                    score_tag: 'P',
-                  },
-                ],
-              },
-            ],
-            segment_list: [
-              {
-                text: 'always happy and helpful',
-                segment_type: 'main',
-                inip: '13',
-                endp: '36',
-                confidence: '100',
-                score_tag: 'P',
-                agreement: 'AGREEMENT',
-                polarity_term_list: [
-                  {
-                    text: 'happy',
-                    inip: '20',
-                    endp: '24',
-                    confidence: '100',
-                    score_tag: 'P',
-                  },
-                  {
-                    text: 'helpful',
-                    inip: '30',
-                    endp: '36',
-                    confidence: '100',
-                    score_tag: 'P',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-        sentimented_entity_list: [],
-        sentimented_concept_list: [
-          {
-            form: 'staff',
-            id: '9431a28626',
-            type: 'Top>Organization',
-            score_tag: 'P',
-          },
-        ],
-      },
-    ],
-    sentimented_entity_list: [],
-    sentimented_concept_list: [
-      {
-        form: 'staff',
-        id: '9431a28626',
-        type: 'Top>Organization',
-        score_tag: 'P',
-      },
-    ],
-  },
-};
-
-function renderAnalyzeReview() {
+function renderAnalyzeReview(reviewResult) {
   const divUpperFeedback = document.getElementById('upper-part-feedback');
   let aspectSerializer = [];
 
-  moreExamples.sentiment_analysis.sentence_list.forEach(sentence_list => {
+  reviewResult.sentiment_analysis.sentence_list.forEach(sentence_list => {
     sentence_list.segment_list.forEach(segment_list => {
       segment_list.polarity_term_list.forEach(polarity_term_list => {
         let typeOfAspect = 'NEU';
@@ -346,12 +228,24 @@ document
     event.preventDefault();
     const textArea = event.target[0];
 
-    textArea.value = '';
+    const postParams = Object.assign(
+      {},
+      {
+        text: textArea.value,
+      },
+    );
 
-    document.getElementById('feedback-text').classList.remove('hidden');
-    document
-      .getElementById('feedback-text')
-      .classList.add('animated', 'fadeInUp');
+    axios
+      .post('', postParams)
+      .then(function(response) {
+        renderAnalyzeReview(response.data);
+        document.getElementById('feedback-text').classList.remove('hidden');
+        document
+          .getElementById('feedback-text')
+          .classList.add('animated', 'fadeInUp');
+        textArea.value = '';
+      })
+      .catch(function(error) {
+        throw new Error(error);
+      });
   });
-
-renderAnalyzeReview();
